d7ced08e708075ac9109291698dcc35a6478dc79
Lưu Thuần Sơn <luuthuansonit@gmail.com>
2020-12-17 00:36:35 +0700

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent , pathMatch: 'full'},
   { path: 'principal', component: PrincipalComponent },
-  { path: '**', component: PagenotfoundComponent }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
