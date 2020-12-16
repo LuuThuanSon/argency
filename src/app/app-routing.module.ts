1fe39008e72a9811550caeba6f029f13e73d34be
Lưu Thuần Sơn <luuthuansonit@gmail.com>
2020-12-17 00:27:14 +0700

    add principal page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { PrincipalComponent } from './principal/principal.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent , pathMatch: 'full'},
+  { path: 'principal', component: PrincipalComponent },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
